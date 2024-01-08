88cd06d83b8f79b28989f107d43953bf8eec3f52
ChhitizeKumar <chhitizeathghara11@gmail.com>
2024-01-08 16:23:00 +0530

    made my utlis

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+//app.use => to set configuration and middlewares
+app.use(express.json({
+    limit: "16kb", //limit incoming json limit to 16KB
+})) 
+
+//jo url ka data aa rha h uska encoding karne ke liye
+//Like space jo url mein aa rha h uska encoding karna(%20, +) ye options h
+//ye express ko batana parta h ki isko configure karo
+//ish configuration ke liye express.urlencoded() use karta h express
+app.use(express.urlencoded({
+    extended: true, //(optional) used for nested object 
+    limit: "16kb" //limit incoming json limit to 16
+}))
+
+
+//static config => if we want to store any files, pdfs, fevicons
+//                 in our server
+app.use(express.static("public"))
+
+//cookieParser() => to perform CRUD opperations on borwer cookies of user
+// meaning to set and access cookies of user
+app.use(cookieParser());
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from './db/index.js';
+import { app } from "./app.js";
+
 
 dotenv.config({
     path: './env'
-})
+});
 
-connectDB(); 
+connectDB()
+.then(() => {
+
+    app.on("error", (error) =>{
+        console.log("Error in App listening: ", error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening on port: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Mongodb connection faied: ", error);
+});
+
