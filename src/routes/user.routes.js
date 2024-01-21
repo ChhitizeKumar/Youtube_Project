37850b509fac414fcb932429d11da43d02489dfc
ChhitizeKumar <chhitizeathghara11@gmail.com>
2024-01-21 23:36:52 +0530

    added and tested all user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken, getUserChannelProfile} from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, getUserChannelProfile, changeCurrentPassword, updateAccountDetails, getCurrentUser, updateUserAvatar, updateUserCoverImage} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,6 +24,12 @@ router.route("/login").post(loginUser);
 //securerd routes
 router.route("/logout").post(verifyJWT ,logoutUser);
 router.route("/refresh-token").post(refreshAccessToken); 
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 
